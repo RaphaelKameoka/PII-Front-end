812ff5a0d71696bb0184df7fbb0bbcb498c11436
Raphael Kameoka <23.01277-3@maua.br>
2023-11-24 17:46:36 -0300

    Animacao adicionada ao abrir e fechar o sidebar

--- Missao_Ambiental/javascript/faca_sua_doacao.js
+++ Missao_Ambiental/javascript/faca_sua_doacao.js
@@ -6,21 +6,21 @@ window.addEventListener("scroll", function(){
 // Sidebar
 
 var header = document.getElementById('header')
-var show_sidebar = false
-function toggleSideBar(){
-    var navigation_header = document.getElementById('navigation_header')
-    show_sidebar = !show_sidebar
-    if (show_sidebar){
-        navigation_header.style.animationName = 'showSidebar'
-        navigation_header.style.marginLeft = '0px' 
-        document.body.style.overflow = 'hidden'
-        
-    }
-    else{
-        navigation_header.style.marginLeft = '-80%'
-        document.body.style.overflow = 'visible'
+var show_sidebar = false;
+function toggleSideBar() {
+    var navigation_header = document.getElementById('navigation_header');
+    show_sidebar = !show_sidebar;
+    if (show_sidebar) {
+        navigation_header.style.animationName = 'showSidebar';
+        navigation_header.style.marginLeft = '0px';
+        document.body.style.overflow = 'hidden';
+    } else {
+        navigation_header.style.animationName = 'hideSidebar';
+        navigation_header.style.marginLeft = '-80%';
+        document.body.style.overflow = 'visible';
     }
 }
+
 function accordion(menuID){
     var menu = document.getElementById(menuID)
     let w = window.innerWidth;
--- Missao_Ambiental/javascript/homeScreenjs.js
+++ Missao_Ambiental/javascript/homeScreenjs.js
@@ -6,19 +6,18 @@ window.addEventListener("scroll", function(){
 // Sidebar
 
 var header = document.getElementById('header')
-var show_sidebar = false
-function toggleSideBar(){
-    var navigation_header = document.getElementById('navigation_header')
-    show_sidebar = !show_sidebar
-    if (show_sidebar){
-        navigation_header.style.animationName = 'showSidebar'
-        navigation_header.style.marginLeft = '0px' 
-        document.body.style.overflow = 'hidden'  
-        
-    }
-    else{
-        navigation_header.style.marginLeft = '-80%'
-        document.body.style.overflow = 'visible'
+var show_sidebar = false;
+function toggleSideBar() {
+    var navigation_header = document.getElementById('navigation_header');
+    show_sidebar = !show_sidebar;
+    if (show_sidebar) {
+        navigation_header.style.animationName = 'showSidebar';
+        navigation_header.style.marginLeft = '0px';
+        document.body.style.overflow = 'hidden';
+    } else {
+        navigation_header.style.animationName = 'hideSidebar';
+        navigation_header.style.marginLeft = '-80%';
+        document.body.style.overflow = 'visible';
     }
 }
 
